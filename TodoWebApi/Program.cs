98906f22ba74d98f59550f451500f354d5dad59b
Hbitz <53080862+Hbitz@users.noreply.github.com>
2025-05-27 09:08:00 +0200

    Added swagger ui

--- TodoWebApi/Program.cs
+++ TodoWebApi/Program.cs
@@ -16,7 +16,7 @@ namespace TodoWebApi
 {
     public class Program
     {
-        public static async void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -50,7 +50,43 @@ namespace TodoWebApi
 
             builder.Services.AddAuthorization(); // For when we want to use [Authorize] requirement on Controller methods
 
+            // Add swagger services
+            builder.Services.AddEndpointsApiExplorer(); // Minimal APIs and better endpoint discovery
+            builder.Services.AddSwaggerGen(); // Swagger generator service
 
+            // Swagger - JWT bearer token authorization
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme.  
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer eyJhbGciOiJIUzI1...'",
+                    Name = "Authorization",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+                {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+            });
 
             // Adds Identity to the container. 
             // Because it injects cookie-based authentication as its default auth scheme and we don't want to use neither that or it's JWT functionality right now, but keep testing with out own first, we comment it away.
@@ -85,6 +121,13 @@ namespace TodoWebApi
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
                 await SeedService.SeedAsync(dbContext);
+            }
+
+            // Only show swagger if in dev
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger(); // Middleware to generate swagger JSON endpoint
+                app.UseSwaggerUI(); // Middleware to serve swagger UI - web page
             }
 
 
